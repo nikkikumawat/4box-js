aa48384c6f3072393d548b8acf589ba06da960e4
nikkikumawat <nikkidigitalmarketer129@gmail.com>
2023-02-03 22:45:02 +0530

    firstcommit

---
+++ 4box.js
@@ -0,0 +1,14 @@
+// let icon = document.querySelectorAll(".box h2")
+// for (let i = 0; i < icon.length; i++) {
+//     icon[i].onclick = function () {
+//         let child = icon[i].ParentElement.children
+//         for (let j = 0; j < child.length; j++) {
+//             if (j === child.length - 1) {
+//                 child[j].style.display = "block"
+//             }
+//             else {
+//                 child[j].style.display = "none"
+//             }
+//         }
+//     }
+// }
